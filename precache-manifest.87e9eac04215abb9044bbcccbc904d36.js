cb180b04879757bbeefefc00ab4c4d7599778f02
Matthew Cheney <matt1cheney@gmail.com>
2020-05-17 16:16:48 -0400

    Updates

---
+++ precache-manifest.87e9eac04215abb9044bbcccbc904d36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc2529e1b91c64dec72dc619b7881c7",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "c054a223cdae0b8a61ed",
+    "url": "/react_portfolio/static/js/2.7e67fbbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_portfolio/static/js/2.7e67fbbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad8fd722ed9abb0bf00",
+    "url": "/react_portfolio/static/js/main.db4ad2e6.chunk.js"
+  },
+  {
+    "revision": "e3e9b940af44bc849f7c",
+    "url": "/react_portfolio/static/js/runtime-main.db4a5409.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_portfolio/Home/precache-manifest.fdce0e45aa1062780b20b69d20d9b1b2.js"
+  "/react_portfolio/precache-manifest.87e9eac04215abb9044bbcccbc904d36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_portfolio/Home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
